2c4ceea923245a6917ef29d0c25776f49be40658
Darko Stosic <splatts@gmail.com>
2016-06-08 23:58:52 +0200

    Dodat migracije osim za anotaciju

---
+++ database/migrations/2016_06_08_212024_create_pictureBook_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePictureBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictureBook', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('authors');
+            $table->string('publisher');
+            $table->string('yearOfPublishing');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pictureBook');
+    }
+}
